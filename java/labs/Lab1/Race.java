9652ea724896ff2d4d034ed2b5dea0b31d546018
Prkizir <A01020382@itesm.mx>
2019-10-22 09:18:45 -0500

    forgot to push aaaah

--- java/labs/Lab1/Race.java
+++ java/labs/Lab1/Race.java
@@ -1,24 +1,59 @@
+/*
+* Lab 1 - Java Concurrency
+* Race.java
+* Sergio Mercado A01020382
+* 14/10/2019
+*/
+
 import java.util.Random;
 
 public class Race{
   public static void main(String[] args) {
     int tortoisePace, harePace;
+    int track_length = 800;
+    int hareRestTime = 100;
 
-    int track_length = 1000;
-    int hareRestTime = 200;
+    //Random-type object creation
 
     Random random = new Random();
 
-    tortoisePace = random.nextInt(8) + 3;
-    harePace = random.nextInt(501) + 500;
+    tortoisePace = random.nextInt((10 - 3) + 1) + 3;
+    harePace = random.nextInt((1000 - 500) + 1) + 500;
+
+    //Racer object creation with modifiable parameters
 
     Racer hare = new Racer("Hare",hareRestTime,harePace,track_length);
     Racer tortoise = new Racer("Tortoise",0,tortoisePace,track_length);
 
+    //Thread object creation
+
     Thread hr = new Thread(hare);
     Thread tr = new Thread(tortoise);
 
-    hr.start();
-    tr.start();
+    try{
+      hr.start(); //Start Hare thread
+      tr.start(); //Start Tortoise thread
+
+      while(hare.getWinner() == false && tortoise.getWinner() == false){
+        hr.join(100);   //These two joins wait for all processes to start
+        tr.join(100);   // at the same time.
+
+        if(hare.getWinner()){ //This checks if there is a winner already if so:
+          System.out.println("Hare won the race");
+          tr.interrupt();      //Interrupt the other (Tortoise) thread.
+          tr.join();           //Wait for process to be killed.
+          break;
+        }
+
+        if(tortoise.getWinner()){ //This checks if there is a winner already if so:
+          System.out.println("Tortoise won the race");
+          hr.interrupt();       //Interrupt the other (Hare) thread.
+          hr.join();            //Wait for process to be killed.
+          break;
+        }
+      }
+    }catch(InterruptedException ie){
+      ie.printStackTrace();
+    }
   }
 }
--- java/labs/Lab1/Racer.java
+++ java/labs/Lab1/Racer.java
@@ -1,10 +1,18 @@
+/*
+* Lab 1 - Java Concurrency
+* Racer.java
+* Sergio Mercado A01020382
+* 14/10/2019
+*/
+
 import java.util.Random;
 
 public class Racer implements Runnable{
-  public static volatile boolean winner = false;
-  private String name;
-  private int restTime, pace, distance;
+  public volatile boolean winner = false; //Volatile boolean variable to check for winner
+  private String name;  //Racer name
+  private int restTime, pace, distance; //Other parameters for each racer
 
+  //Constructor
   public Racer(String name, int restTime, int pace, int distance){
     this.name = name;
     this.restTime = restTime;
@@ -12,23 +20,41 @@ public class Racer implements Runnable{
     this.distance = distance;
   }
 
+
+  //Run method for the actual race
   @Override
   public void run(){
     int i = 0;
 
-    for(i = 0; i < distance; i+=pace){
+    //Runs as long as it's not interrupted and the current distance ran is less than
+    // the total distance of the race
+    while(!Thread.currentThread().isInterrupted() && i < distance){
       System.out.println(name + " is at mile # " + i);
+      i += pace; //Pace increments
+
+      //Once a racer gets to the race distance exit the loop
+      if(i >= distance){
+        break;
+      }
+
       try{
-        if( name.equals("Hare") &&
-            ((i % pace) == 0) && (i > 0)){
+        if(name.equals("Hare") && ((i % pace) == 0) && (i != 0)){  //Checks for the Hare pace so that it rests every N miles it runs
           System.out.println("The Hare is sleeping at mile no. " + i);
-          Thread.sleep(restTime);
+          Thread.sleep(restTime);  //Sleep the thread
         }
-      }catch(InterruptedException ie){
-        ie.printStackTrace();
+      }catch(InterruptedException ie){ //Catch an external interruption
+        System.out.println(name + " finished the race at " + i + " mile"); //Print the current state of any racer
+        Thread.currentThread().interrupt();
       }
     }
 
-    System.out.println("Winner is " + name);
+    //Set the winner variable to true because the Racer finished the race.
+    winner = true;
   }
+
+  //winner boolean variable getter
+  public boolean getWinner(){
+    return winner;
+  }
+
 }
